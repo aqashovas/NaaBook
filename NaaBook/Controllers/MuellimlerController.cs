653082fff02291ad80f40887425dc74facaa07b3
Sabina Aqashova <aqashova@inbox.ru>
2019-05-10 17:35:58 +0400

    TeacherListandSidebarmenu

---
+++ NaaBook/Controllers/MuellimlerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NaaBook.Controllers
+{
+    public class MuellimlerController : Controller
+    {
+        // GET: Muellimler
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
